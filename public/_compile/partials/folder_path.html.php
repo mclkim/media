889dc5a02c05a2b248f3b4d8e54f3fa79df81b97
김명철 <mclkim@blueweb.co.kr>
2016-02-04 10:42:38 +0900

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   .gitmodules
    	deleted:    public/_compile/layouts/default.html.php
    	deleted:    public/_compile/layouts/head.html.php
    	deleted:    public/_compile/layouts/mainmenu.html.php
    	deleted:    public/_compile/partials/body.html.php
    	deleted:    public/_compile/partials/bottom_toolbar.html.php
    	deleted:    public/_compile/partials/folder_path.html.php
    	deleted:    public/_compile/partials/folder_toolbar.html.php
    	deleted:    public/_compile/partials/item_list.html.php
    	deleted:    public/_compile/partials/left_sidebar.html.php
    	deleted:    public/_compile/partials/new_folder_form.html.php
    	deleted:    public/_compile/partials/right_sidebar.html.php
    	deleted:    public/_compile/partials/toolbar.html.php
    	deleted:    public/_compile/partials/upload_progress.html.php
    	new file:   public/_template/assets/js/jquery-1.12.0.min.js
    	modified:   public/_template/layouts/head.html
    	modified:   public/index.php

--- public/_compile/partials/folder_path.html.php
+++
@@ -1,8 +0,0 @@
-<?php /* Template_ 2.2.8 2016/02/03 10:45:46 D:\phpdev\workspace\media\public\_template\partials\folder_path.html 000000414 */ ?>
-<ul class="tree-path">
-	<li class="root"><a href="#" data-type="media-item"
-		data-item-type="folder" data-path="/" data-clear-search="true">라이브러리</a></li>
-
-	<li><a href="#" data-type="media-item" data-item-type="folder"
-		data-path="/Folder Test">Folder Test</a></li>
-</ul>
